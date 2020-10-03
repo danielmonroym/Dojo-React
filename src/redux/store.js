31be12538a9ec4b4381e84aeaf8ac8d85c4d2ede
daniel.monroy <daniel.monroym@udea.edu.co>
2020-10-03 09:36:29 -0500

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import {gamesReducer, SearchReducer } from './reducers'
+
+// DefaultState
+export const defaultGamesState = {
+    pokemon: 10,
+}
+export const initialStateSearch = {
+    loading: false,
+    pokemon: [],
+    error: ''
+}
+
+// Store
+const rootReducers = combineReducers({
+    games: gamesReducer,
+    seeker: SearchReducer
+})
+
+
+
+// copiar esta linea si no se instalo redux devtools
+const store = createStore(rootReducers, applyMiddleware(thunk)
+)
+
+export default store;
